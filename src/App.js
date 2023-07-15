55f73e390b7b765f77f434ced145694d9522c41c
Ronald Sigei <ronald@pixsel.net>
2023-07-15 10:17:49 +0300

    react inventory

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import SearchBar from './SearchBar';  
+import AddItem from './AddItem';
+import ItemsDisplay from './ItemsDisplay';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [filters, setFilters] = useState({});
+  const [data, setData] = useState({ items: [] });
+
+  const updateFilters = (searchParams) => {
+    setFilters(searchParams);
+  }
+
+  const addItemToData = (item) => {
+    let items = data["items"];
+    item.id = items.length;
+    items.push(item);
+    setData({ items: items });
+    console.log(data);
+  }
+
+
+  const filterData = (data) => {
+    const filteredData = [];
+
+    if (!filters.name) {
+      return data;
+    }
+
+    for (const item of data) {
+      if (filters.name !== "" && item.name !== filters.name) {
+        continue;
+      }
+    
+      if (filters.price !== 0 && item.price > filters.price) {
+        continue
+      }
+      if (filters.type !== "" && item.type !== filters.type) {
+        continue
+      }
+      if (filters.brand !== "" && item.brand !== filters.brand) {
+        continue
+      }
+      filteredData.push(item);
+    }
+
+      return filteredData;
+    }
+    return (
+      <div className="container">
+        <div className='row mt-3'>
+          <ItemsDisplay items={filterData(data["items"])} />
+        </div>
+        <div className='row mt-3'>
+          <SearchBar updateSearchParams={updateFilters} />
+        </div>
+        <div className='row mt-3'>
+          <AddItem addItem={addItemToData} />
+        </div>
+      </div>
+    );
+  
 }
 
 export default App;
